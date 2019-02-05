4e27f7f28a3a1fb894eef7bc916970fc5b82ed47
dtkocher <dtkocher@gmail.com>
2019-02-05 11:18:56 -0500

    Break out updating of task to make code cleaner.

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,15 +1,19 @@
 import React, { useState, useContext } from 'react'
+import _ from 'lodash'
 import SubTaskList from './subTaskList'
 import { GlobalContext } from './context/globalContext'
 
 const Task = (props) => {
   const context = useContext(GlobalContext)
   const [expand, setExpand] = useState(false)
+  const task = context.tasks[props.taskIdx]
+
+  const unfinished = _.filter(task.subTasks, (subTask) => { return !subTask.complete })
 
   return (
     <li className="list-group-item">
       <div onClick={() => setExpand(expand => !expand)}>
-        {context.tasks[props.taskIdx].name}
+        {task.name} - {unfinished.length} Unfinished
       </div>
       { expand && (<SubTaskList taskIdx={props.taskIdx}/>) }
     </li>
